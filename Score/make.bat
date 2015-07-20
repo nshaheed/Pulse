fe3816a61edeb2d0c4dd604ee57bdec054a18c05
nshaheed <n704s313@ku.edu>
2015-07-20 13:20:07 -0500

    Finished first draft of preface added preliminaries-book to the make file

--- Score/make.bat
+++ Score/make.bat
@@ -2,8 +2,13 @@ REM Compiles both the 10"x13" & 8.5"x11" versions of the score
 lilypond -dno-point-and-click -ddelete-intermediate-files -dinclude-settings=fullscoressettings.ly --pdf full_score.ly
 lilypond -dno-point-and-click -ddelete-intermediate-files --pdf test_size.ly
 
+REM compiles the forward
+C:\Python27\python.exe "C:\Program Files (x86)\LilyPond\usr\bin\lilypond-book.py" --pdf --output=dir preliminaries-book.lytex
+rem C:\Python27\python.exe "C:\Program Files (x86)\LilyPond\usr\bin\lilypond-book.py" --pdf preliminaries-book.lytex
+xelatex.exe -include-directory=DIR dir\preliminaries-book.tex
+
 REM Compiles the title page and info pages
 xelatex.exe title.tex
 
 REM Combine the title page & 10" x 13" score
-pdftk.exe title.pdf full_score.pdf cat output pulse.pdf
+pdftk.exe title.pdf preliminaries-book.pdf full_score.pdf cat output pulse.pdf
